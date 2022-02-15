9ab9cd216e271f23ea34b515dd18ee7d506c5526
Mahtab Hassan <50697282+mahtab2003@users.noreply.github.com>
2022-02-15 19:48:02 +0500

    Add files via upload

---
+++ src/Exception/AuthorizationException.php
@@ -0,0 +1,8 @@
+<?php 
+namespace Mahtab2003\Mofh\Exception;
+
+class AuthorizationException extends \Exception
+{
+
+}
+?>
---
+++ src/Exception/InvalidRequestException.php
@@ -0,0 +1,8 @@
+<?php 
+namespace Mahtab2003\Mofh\Exception;
+
+class InvalidRequestException extends \Exception
+{
+
+}
+?>
